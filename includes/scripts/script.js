7f90720481f718c4160567d68c237c722507bff2
Dylan M <dylanmaertens@gmail.com>
2022-11-10 10:49:03 +0100

    Modif front et back + formulaire de contact à revoir

--- includes/scripts/script.js
+++ includes/scripts/script.js
@@ -1,45 +1,66 @@
 console.log("Hello World !");
 
-let menu1 = document.querySelector(".menu1");
-let menu2 = document.querySelector(".menu2");
-let menu3 = document.querySelector(".menu3");
+let time = new Date();
+window.addEventListener('DOMContentLoaded',()=>{
+    console.log('loaded');
+    
+    let minute = time.getMinutes();
+    let heure = time.getHours();
+    let jour = time.getDay() + 6;
+    let mois = time.getMonth() + 1;
+    let annee = time.getFullYear();
+    
+    minute = (minute < 10) ? "0" + minute : minute;
+    heure = (heure < 10) ? "0" + heure : heure;
+    jour = (jour < 10) ? "0" + jour : jour;
+    mois = (mois < 10) ? "0" + mois : mois;
+
+    document.querySelector('#dateReservation').min = (annee + '-' + mois + '-' + jour + 'T' + heure + ':' + minute);
+    document.querySelector('#dateReservation').value = (annee + '-' + mois + '-' + jour + 'T' + heure + ':' + minute);
+})
+
+let menu1 = document.querySelector("#menu1");
+let menu2 = document.querySelector("#menu2");
+let menu3 = document.querySelector("#menu3");
 let contactBtn = document.querySelector('#contactBtn');
 let fermerFormBtn = document.querySelector('#fermerFormBtn');
 let contactForm = document.querySelector('#contactForm');
 let bgImage = document.getElementById('image');
 
+
 menu1.addEventListener('click',()=>{
-    if(menu1.classList.value == 'menu1'){
+    console.log("menu1");
+    if(menu1.classList.value == 'menu'){
         bgImage.style.backgroundImage = "url(includes/images/image1.png)";
         menu1.classList.add("active");
         menu2.classList.remove('active');
         menu3.classList.remove('active');
     }else{
-        bgImage.style.backgroundImage = "url(https://source.unsplash.com/random/900×700/?spa,night)";
+        bgImage.style.backgroundImage = "url(includes/images/imageDefaut.jpg)";
         menu1.classList.remove("active");
     }
 })
 
 menu2.addEventListener('click',()=>{
-    if(menu2.classList.value == 'menu2'){
+    if(menu2.classList.value == 'menu'){
         bgImage.style.backgroundImage = "url(includes/images/image2.png)";
-        menu2.classList.add("active");
         menu1.classList.remove("active");
+        menu2.classList.add("active");
         menu3.classList.remove("active");
     }else{
-        bgImage.style.backgroundImage = "url(https://source.unsplash.com/random/900×700/?night,spa)";
+        bgImage.style.backgroundImage = "url(includes/images/imageDefaut.jpg)";
         menu2.classList.remove("active");
     }
 })
 
 menu3.addEventListener('click',()=>{
-    if(menu3.classList.value == 'menu3'){
+    if(menu3.classList.value == 'menu'){
         bgImage.style.backgroundImage = "url(includes/images/image3.png)";
-        menu2.classList.remove("active");
         menu1.classList.remove("active");
+        menu2.classList.remove("active");
         menu3.classList.add("active");
     }else{
-        bgImage.style.backgroundImage = "url(https://source.unsplash.com/random/900×700/?spa,boobs)";
+        bgImage.style.backgroundImage = "url(includes/images/imageDefaut.jpg)";
         menu3.classList.remove("active");
     }
 })
@@ -55,4 +76,24 @@ contactBtn.addEventListener('click',()=>{
 
 fermerFormBtn.addEventListener('click',()=>{
     contactForm.style.display = "none";
+})
+
+let reserve1 = document.querySelector('#reservation1');
+let reserve2 = document.querySelector('#reservation2');
+let reserve3 = document.querySelector('#reservation3');
+let prestationSelectionner = document.querySelector('#prestation');
+
+reserve1.addEventListener('click',()=>{
+    contactForm.style.display = "block";
+    prestationSelectionner.value = "prestation 1"
+})
+
+reserve2.addEventListener('click',()=>{
+    contactForm.style.display = "block";
+    prestationSelectionner.value = "prestation 2"
+})
+
+reserve3.addEventListener('click',()=>{
+    contactForm.style.display = "block";
+    prestationSelectionner.value = "prestation 3"
 })
